1a95a635e738da23bc9ded4316c6503e15131426
Bowen Baker <bowen@mit.edu>
2014-04-26 13:38:35 -0400

    MotorControllerAbstract

---
+++ groovy2014/src/Motor/src/MotorController.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+  
+using namespace std;
+
+class MotorController {
+	private:
+    	bool status = true;
+        string deviceID;
+  	public:
+  		//getter methods
+		virtual double getVelocity() =0;
+  		virtual double getPosition() =0;
+  		virtual double getCurrent() =0;
+  		//setter methods :
+  		//		return true if successfully set parameter
+  		//		return false otherwise
+  		virtual void setVelocity(double vel) =0;
+  		virtual void setPosition(double pos) =0;
+  		virtual void setCurrent(double current) =0;
+  		//returns status
+  		virtual bool getStatusBool() =0;
+  		//returns error message or empty string
+  		virtual string getStatusString() =0;
+  		//returns true if successfully connected
+  		virtual bool connectToDevice(string devID) =0;
+}
