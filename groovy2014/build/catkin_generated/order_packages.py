c7dc39855060cf44c9df511d14265d86de421c8a
Roboops <roboops@Rover.(none)>
2014-04-26 13:14:18 -0400

    reorganize

---
+++ groovy2014/build/catkin_generated/order_packages.py
@@ -0,0 +1,4 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/roboops/2014Software/groovy2014/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
---
+++ groovy2014/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/roboops/2014Software/groovy2014/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/roboops/2014Software/groovy2014/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/roboops/2014Software/groovy2014/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/roboops/2014Software/groovy2014/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/roboops/2014Software/groovy2014/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/home/roboops/2014Software/groovy2014/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/roboops/2014Software/groovy2014/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/roboops/2014Software/groovy2014/build"
+export PYTHONPATH="/home/roboops/2014Software/groovy2014/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/roboops/2014Software/groovy2014/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/roboops/2014Software/groovy2014/src:/opt/ros/groovy/share:/opt/ros/groovy/stacks"
